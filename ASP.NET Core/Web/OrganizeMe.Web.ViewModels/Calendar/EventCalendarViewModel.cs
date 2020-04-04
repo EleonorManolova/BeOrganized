6684bcbb86b388a09d9899ba47420e5204e63c2f
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-04-04 15:31:39 +0300

    Updated packages and .Net core 3.1.2

--- ASP.NET Core/Web/OrganizeMe.Web.ViewModels/Calendar/EventCalendarViewModel.cs
+++ ASP.NET Core/Web/OrganizeMe.Web.ViewModels/Calendar/EventCalendarViewModel.cs
@@ -1,11 +1,12 @@
 ﻿namespace OrganizeMe.Web.ViewModels.Calendar
 {
     using System;
+
     using AutoMapper;
     using OrganizeMe.Data.Models;
     using OrganizeMe.Services.Mapping;
 
-    public class EventCalendarViewModel : IMapFrom<Event>, IHaveCustomMappings
+    public class EventCalendarViewModel : IHaveCustomMappings
     {
         public string Title { get; set; }
 
@@ -19,10 +20,15 @@
 
         public void CreateMappings(IProfileExpression configuration)
         {
-
             configuration.CreateMap<Event, EventCalendarViewModel>().ForMember(
                 m => m.Title,
                 opt => opt.MapFrom(x => x.Title));
+
+            configuration.CreateMap<Event, EventCalendarViewModel>()
+                .ForMember(x => x.StartDate, y => y.MapFrom(x => x.StartDateTime.Date))
+                .ForMember(x => x.StartTime, y => y.MapFrom(x => default(DateTime).Add(x.StartDateTime.TimeOfDay)))
+                .ForMember(x => x.EndDate, y => y.MapFrom(x => x.EndDateTime.Date))
+                .ForMember(x => x.EndTime, y => y.MapFrom(x => default(DateTime).Add(x.EndDateTime.TimeOfDay)));
         }
     }
 }
