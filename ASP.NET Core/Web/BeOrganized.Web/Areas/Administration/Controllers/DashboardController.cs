e06d580212044c5a2c2f5620e33bb39b08ebcd88
Eleonor Manolova <eleonor.manolova@gmail.com>
2020-04-24 22:33:56 +0300

    Update Dashboad

--- ASP.NET Core/Web/BeOrganized.Web/Areas/Administration/Controllers/DashboardController.cs
+++ ASP.NET Core/Web/BeOrganized.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,27 +1,37 @@
 ﻿namespace BeOrganized.Web.Areas.Administration.Controllers
 {
-    using BeOrganized.Services.Data;
+    using System;
+    using System.Linq;
+
+    using BeOrganized.Data.Models;
+    using BeOrganized.Services.Data.Events;
     using BeOrganized.Web.ViewModels.Administration.Dashboard;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
 
     public class DashboardController : AdministrationController
     {
-        private readonly ISettingsService settingsService;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IEventService eventService;
 
-        public DashboardController(ISettingsService settingsService)
+        public DashboardController(UserManager<ApplicationUser> userManager, IEventService eventService)
         {
-            this.settingsService = settingsService;
+            this.userManager = userManager;
+            this.eventService = eventService;
         }
 
         public IActionResult Index()
         {
+            var newUsers = this.userManager.Users.Where(x => x.CreatedOn.Date == DateTime.Now.Date).Count();
+            var newUsersForMonth = this.userManager.Users.Where(x => x.CreatedOn.Date == DateTime.Now.AddMonths(-1).Date).Count();
+            var eventsCreatedToday = this.eventService.GetAll().Where(x => x.CreatedOn.Date == DateTime.Now.Date).Count();
             var viewModel = new IndexViewModel
             {
-                SettingsCount = this.settingsService.GetCount(),
-                UserName = this.User.Identity.Name,
-                UniqueVisitorsCount = 5,
+                NewEventsToday = eventsCreatedToday,
+                NewUsersThisMonth = newUsersForMonth,
+                NewUsersToday = newUsers,
             };
-            return this.View();
+            return this.View(viewModel);
         }
     }
 }
